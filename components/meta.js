ca8ff3cb4328aae58ee348c7e86d410b5dccf47c
liyinggao <344626033@qq.com>
2021-03-04 20:35:57 +0800

    test markdown image

--- components/meta.js
+++ components/meta.js
@@ -34,7 +34,7 @@ export default function Meta() {
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+        content="赢者篮球官方文档"
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
